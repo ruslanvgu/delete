957862324ece80559332ac8877eb085afa971dd5
ruslanvgu <ruslanvgu@mail.ru>
2023-09-25 17:58:42 +0300

    include timer

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include "timer.h"
+
 int main(int argc, char** argv){
+
+	{
+	Timer tm;
 
 	for(int i=0; i<argc; ++i){
 		std::cout <<i<<":"<< argv[i] <<std::endl;
 	}
-	
+	}
+
 	return 0;
 }
